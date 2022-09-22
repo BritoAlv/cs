1937233cbdf225c492282cc33aac11efcdd86aab
BritoAlv <gomzalezbrito@gmail.com>
2022-09-22 14:52:44 -0400

    Backtracking

---
+++ Permutaciones/Program.cs
@@ -0,0 +1,36 @@
+﻿class permutacion
+{
+    public static void Main()
+    {
+        List<int> numbers = new List<int>(){2,3,4,5,6};
+        bool[] taken = new bool[numbers.Count];
+        permutaciones("", taken,  numbers);
+    }
+
+
+    /*
+    given a list of numbers (there may be repeated ones return all its permutaciones) 
+    */
+    public static void permutaciones( string actual_permutation, bool[] taken, List<int> numbers )
+    {
+        if(actual_permutation.Length == taken.Length)
+        {
+            Console.WriteLine(actual_permutation);
+            return;
+        }
+        else
+        {
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (taken[i] == false)
+                {
+                    taken[i] = true;
+                    actual_permutation = actual_permutation + numbers[i];
+                    permutaciones(actual_permutation, taken, numbers);
+                    actual_permutation = actual_permutation.Substring(0, actual_permutation.Length-1);
+                    taken[i] = false;                    
+                }
+            }
+        }
+    }
+}
